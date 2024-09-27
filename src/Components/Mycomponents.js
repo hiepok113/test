699cea3c910f7ddb6df3d1ad9cfe7a78364e406d
hiepok113 <thanhhiep.hongduc@gmail.com>
2024-09-28 01:29:05 +0700

    bai 16

---
+++ src/Components/Mycomponents.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Mycomponents extends React.Component {
+    render() {
+      return (
+        <div>
+          hello words 
+          {Math.random()}
+        </div>
+      );
+    }
+  }
+  
+  export default Mycomponents;
+  
