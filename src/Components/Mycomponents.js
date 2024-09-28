6f0369614127d1ccee6ebfed4a605e84caf6bb40
hiepok113 <thanhhiep.hongduc@gmail.com>
2024-09-28 14:39:37 +0700

    bai17

--- src/Components/Mycomponents.js
+++ src/Components/Mycomponents.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 
 class Mycomponents extends React.Component {
-    render() {
-      return (
-        <div>
-          hello words 
-          {Math.random()}
-        </div>
-      );
-    }
+  state = {
+    name: 'NTH',
+    address: 'tphcm',
+    age: 25
+  };
+  render() {
+    return (
+      <div>
+        hello words
+        my name is {this.state.name} come from {this.state.address}
+      </div>
+    );
   }
-  
-  export default Mycomponents;
-  
+}
+
+export default Mycomponents;
