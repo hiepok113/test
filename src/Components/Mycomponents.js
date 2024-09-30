6541eb1042f7cad011f246414a711ec4256b6443
hiepok113 <thanhhiep.hongduc@gmail.com>
2024-09-30 16:26:33 +0700

    ngay 30

--- src/Components/Mycomponents.js
+++ src/Components/Mycomponents.js
@@ -7,11 +7,26 @@ class Mycomponents extends React.Component {
     address: 'tphcm',
     age: 25
   };
+  handleClick(){
+    console.log(">>click me my button");
+    //merge state =>react class
+    this.setState({
+      name:'hiepok',
+      age  : Math.floor((Math.random()*100)+1)
+    })
+  
+
+  }
+  handleOnMoverOver(event){
+    // console.log(event.pageX)
+  }
   render() {
     return (
       <div>
         hello words
-        my name is {this.state.name} come from {this.state.address}
+        my name is {this.state.name} come from {this.state.address} and im {this.state.age}
+        <button onMouseOver={this.handleOnMoverOver}>hover me </button>
+        <button onClick={(event)=>{this.handleClick()}}>click me </button>
       </div>
     );
   }
